5da5f6b19c5af7b5340514b0343592e44d9d073c
dani <dinulhaqhamdani41@gmail.com>
2025-01-02 23:56:01 +0700

    add responsiveness

--- src/pages/BankAccount.tsx
+++ src/pages/BankAccount.tsx
@@ -1,7 +1,15 @@
-import { CreditCard, Copy, Scan } from 'lucide-react'
+import { CreditCard, Copy, Scan, Check } from 'lucide-react'
+
+// hooks
+import { useState } from 'react'
+
+// assets
+import qrisPict from '../assets/images/qris_eyip.jpg'
 
 function BankAccount() {
-    let accounts = [
+    const [copied, setCopied] = useState(false)
+
+    const accounts = [
         {
             bank: 'BCA',
             account_no: '553-504-0777',
@@ -29,11 +37,11 @@ function BankAccount() {
     ]
 
     const accountsCard = accounts.map((item, index) => {
-        return <div key={'account-number-' + index} className="flex items-center justify-between bg-indigo-900 bg-opacity-40 rounded-lg px-4 py-2 backdrop-filter backdrop-blur-sm mb-2">
-            <div className="grid grid-cols-[4vw_1vw_12vw] gap-4">
-                <span className="text-[1vw] font-mono font-bold text-teal-100">{item.bank}</span>
-                <span className="text-[1vw] font-mono font-bold text-teal-100">:</span>
-                <span className="text-[1vw] font-mono font-bold text-teal-100">{item.account_no}</span>
+        return <div key={'account-number-' + index} className="flex items-center justify-between bg-indigo-900 bg-opacity-40 rounded-lg px-4 py-1 xl:py-2 backdrop-filter backdrop-blur-sm mb-2">
+            <div className="grid grid-cols-[5rem_1rem_10rem] xl:grid-cols-[4vw_1vw_10vw] gap-4">
+                <span className="text-md xl:text-[1vw] font-mono font-bold text-teal-100">{item.bank}</span>
+                <span className="text-md xl:text-[1vw] font-mono font-bold text-teal-100">:</span>
+                <span className="text-md xl:text-[1vw] font-mono font-bold text-teal-100">{item.account_no}</span>
             </div>
             <button
                 onClick={() => copyToClipboard(item.account_no)}
@@ -46,17 +54,15 @@ function BankAccount() {
     })
 
     const copyToClipboard = (numb: string) => {
-        try {
-            navigator.clipboard.writeText(numb)
-        } catch (e) {
-            console.log(e)
-        }
+        navigator.clipboard.writeText(numb)
+        setCopied(true)
+        setTimeout(() => setCopied(false), 2000)
     }
 
     return (
         <>
             <div className="min-h-screen w-screen flex items-center justify-center p-4 bg-gradient-to-br from-teal-700 via-indigo-700 to-purple-800">
-                <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl shadow-lg p-8 max-w-lg w-full">
+                <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl shadow-lg p-8 max-w-md w-full">
                     <div className="flex justify-between items-center mb-8">
                         <h1 className="text-3xl font-bold text-teal-200">Nomor Rekening</h1>
                         <div className="w-16 h-16 bg-indigo-400 rounded-full flex items-center justify-center shadow-lg">
@@ -66,15 +72,18 @@ function BankAccount() {
                     <p className="text-sm text-teal-300 mb-2 font-semibold">Salin nomor berikut</p>
 
                     {accountsCard}
-                </div>
-                <div className="flex flex-col items-center mb-8">
-                    <div className="bg-gradient-to-r from-teal-500 to-indigo-500 p-4 rounded-2xl shadow-lg mb-4 transform hover:scale-105 transition-transform duration-200">
-
+                    <div className="flex flex-col items-center mb-1 mt-8">
+                        <div className="bg-gradient-to-r from-teal-500 to-indigo-500 p-4 rounded-2xl shadow-lg mb-4 w-fit transform hover:scale-105 transition-transform duration-200">
+                            <img src={qrisPict} alt="Qris Image" className='w-[10rem] h-[10rem] xl:w-[10vw] xl:h-[10vw]' />
+                        </div>
+                        <p className="text-sm text-teal-200 flex items-center mb-2">
+                            <Scan className="w-4 h-4 mr-2" />
+                            Pindai untuk pembayaran menggunakan metode QRIS
+                        </p>
+                        {copied && <div className='animate-popFade py-1 px-2 rounded-md bg-cyan-950 flex items-center'>
+                            <Check className='mr-2 w-[1rem] xl:w-[0.8vw]' /> <span className='font-medium text-xs'>Berhasil menyalin nomor rekening</span>
+                        </div>}
                     </div>
-                    <p className="text-sm text-teal-200 flex items-center">
-                        <Scan className="w-4 h-4 mr-2" />
-                        Scan for quick and secure payment
-                    </p>
                 </div>
             </div>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS, JSX, TS, and TSX files in the src folder
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        popFade: {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(1.1)', opacity: '0.8' },
+          '100%': { transform: 'scale(0.9)', opacity: '0' },
+        },
+      },
+      animation: {
+        popFade: 'popFade 2s forwards',
+      },
+    },
   },
   plugins: [],
 };
